76cfc58808fc3f4931b9693defddeb4071e45f1d
Harmeet Singh Bhatia <h@thedrone7.dev>
2025-07-14 00:23:13 -0400

    Add background image

--- lib/widgets/home/home.dart
+++ lib/widgets/home/home.dart
@@ -14,64 +14,77 @@ class NavigationHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final AppState state = Provider.of<AppState>(context);
 
-    return Scaffold(
-      key: state.scaffoldKey,
-      drawer: Observer(
-        builder: (_) => NavigationDrawer(
-          onDestinationSelected: state.handleMainMenuChange,
-          selectedIndex: state.screenIndex,
-          children: [
-            Padding(
-              padding: EdgeInsetsGeometry.fromLTRB(16, 12, 16, 0),
-              child: Text(
-                'Persona Assistant',
-                style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                  fontWeight: FontWeight.w400,
+    return Stack(
+      children: [
+        Image.asset(
+          'assets/images/bg1.jpg',
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
+        ),
+        Scaffold(
+          key: state.scaffoldKey,
+          backgroundColor: Colors.transparent,
+          drawer: Observer(
+            builder: (_) => NavigationDrawer(
+              onDestinationSelected: state.handleMainMenuChange,
+              selectedIndex: state.screenIndex,
+              children: [
+                Padding(
+                  padding: EdgeInsetsGeometry.fromLTRB(16, 12, 16, 0),
+                  child: Text(
+                    'Persona Assistant',
+                    style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
                 ),
+                Padding(
+                  padding: EdgeInsetsGeometry.fromLTRB(0, 8, 0, 8),
+                  child: Divider(),
+                ),
+                ...destinations.map((dest) {
+                  return NavigationDrawerDestination(
+                    label: Text(dest.label),
+                    icon: dest.icon,
+                    selectedIcon: dest.selectedIcon,
+                  );
+                }),
+              ],
+            ),
+          ),
+          appBar: AppBar(
+            leading: IconButton(
+              icon: const FaIcon(FontAwesomeIcons.bars),
+              onPressed: state.openDrawer,
+            ),
+            title: Observer(
+              builder: (_) => Text(destinations[state.screenIndex].label),
+            ),
+            actions: [
+              IconButton(
+                icon: const FaIcon(FontAwesomeIcons.gear),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => UnlockSettingsPage(),
+                    ),
+                  );
+                },
               ),
+            ],
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(48.0),
+              child: Observer(builder: (_) => state.currentFilters),
             ),
-            Padding(
-              padding: EdgeInsetsGeometry.fromLTRB(0, 8, 0, 8),
-              child: Divider(),
-            ),
-            ...destinations.map((dest) {
-              return NavigationDrawerDestination(
-                label: Text(dest.label),
-                icon: dest.icon,
-                selectedIcon: dest.selectedIcon,
-              );
-            }),
-          ],
-        ),
-      ),
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const FaIcon(FontAwesomeIcons.bars),
-          onPressed: state.openDrawer,
-        ),
-        title: Observer(
-          builder: (_) => Text(destinations[state.screenIndex].label),
-        ),
-        actions: [
-          IconButton(
-            icon: const FaIcon(FontAwesomeIcons.gear),
-            onPressed: () {
-              Navigator.of(
-                context,
-              ).push(MaterialPageRoute(builder: (context) => UnlockSettingsPage()));
+          ),
+          body: Observer(
+            builder: (_) {
+              return state.currentScreen;
             },
           ),
-        ],
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(48.0),
-          child: Observer(builder: (_) => state.currentFilters),
         ),
-      ),
-      body: Observer(
-        builder: (_) {
-          return state.currentScreen;
-        },
-      ),
+      ],
     );
   }
 }
