e9d3f61680bf1352f0ccdac4391c612260d9673c
Harmeet Singh Bhatia <h@thedrone7.dev>
2025-07-14 06:48:34 -0400

    Divide sort and filter further

--- lib/widgets/common/sort_and_filter_controls.dart
+++ lib/widgets/common/sort_and_filter_controls.dart
@@ -4,6 +4,10 @@ import 'package:persona_assistant/types/filters.dart';
 
 import 'filters_button.dart';
 
+/// A widget that provides controls for sorting and filtering using modal bottom sheets.
+///
+/// Displays two buttons: one for sorting and one for filtering. When pressed, each button
+/// opens a modal with the respective options.
 class SortAndFilterControls extends StatelessWidget {
   final Widget sortIcon;
   final String sortLabel;
@@ -56,6 +60,7 @@ class SortAndFilterControls extends StatelessWidget {
     );
   }
 
+  /// Shows a modal bottom sheet with sort options.
   void _showSortOptions(
     BuildContext context,
     List<SortOption> options,
@@ -64,29 +69,12 @@ class SortAndFilterControls extends StatelessWidget {
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
-        return ListView.builder(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20.0),
-          itemCount: options.length,
-          itemBuilder: (context, index) {
-            final option = options[index];
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FiltersButton(
-                key: Key(option.value),
-                icon: option.icon,
-                onPressed: () {
-                  onChanged(option);
-                  Navigator.pop(context);
-                },
-                child: Text(option.label.toUpperCase()),
-              ),
-            );
-          },
-        );
+        return _SortOptionsModal(options: options, onChanged: onChanged);
       },
     );
   }
 
+  /// Shows a modal bottom sheet with filter options.
   void _showFilterOptions(
     BuildContext context,
     List<FilterOption> options,
@@ -95,23 +83,67 @@ class SortAndFilterControls extends StatelessWidget {
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
-        return ListView.builder(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20.0),
-          itemCount: options.length,
-          itemBuilder: (context, index) {
-            final option = options[index];
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FiltersButton(
-                key: Key(option.value),
-                onPressed: () {
-                  onChanged(option);
-                  Navigator.pop(context);
-                },
-                child: Text(option.label.toUpperCase()),
-              ),
-            );
-          },
+        return _FilterOptionsModal(options: options, onChanged: onChanged);
+      },
+    );
+  }
+}
+
+/// Modal widget for displaying sort options.
+class _SortOptionsModal extends StatelessWidget {
+  final List<SortOption> options;
+  final void Function(SortOption) onChanged;
+
+  const _SortOptionsModal({required this.options, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20.0),
+      itemCount: options.length,
+      itemBuilder: (context, index) {
+        final option = options[index];
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FiltersButton(
+            key: Key(option.value),
+            icon: option.icon,
+            onPressed: () {
+              onChanged(option);
+              Navigator.pop(context);
+            },
+            child: Text(option.label.toUpperCase()),
+          ),
+        );
+      },
+    );
+  }
+}
+
+/// Modal widget for displaying filter options.
+class _FilterOptionsModal extends StatelessWidget {
+  final List<FilterOption> options;
+  final void Function(FilterOption) onChanged;
+
+  const _FilterOptionsModal({required this.options, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20.0),
+      itemCount: options.length,
+      itemBuilder: (context, index) {
+        final option = options[index];
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FiltersButton(
+            key: Key(option.value),
+            onPressed: () {
+              onChanged(option);
+              Navigator.pop(context);
+            },
+            child: Text(option.label.toUpperCase()),
+          ),
         );
       },
     );
